a624163190182c2c131753bbc40878b7ced1b2bc
letoro99 <atgmatias.12@gmail.com>
2022-04-03 01:12:06 -0400

    movimiento con gravedad del PJ

--- AstroRivals/scenes/Gravity.gd
+++ AstroRivals/scenes/Gravity.gd
@@ -1,12 +1,16 @@
 extends Area2D
 
 onready var planet = get_parent()
-var GRAVITY_FORCE = 100
+var GRAVITY_FORCE = 200
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	global_position = planet.global_position
 	gravity = GRAVITY_FORCE
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_pressed("add_grav"):
+		gravity += 100
+	elif Input.is_action_pressed("low_grav"):
+		gravity -= 100
