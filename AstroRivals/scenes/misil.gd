92b33cc9e95e3090214f42adab54246cd71db3c2
salveal@github.com <salvador.alveal@ug.uchile.cl>
2022-05-02 16:48:32 -0400

    bullets now follow velocity direction

--- AstroRivals/scenes/misil.gd
+++ AstroRivals/scenes/misil.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 # Variables
 var grav_point = Vector2()
+var b_rotation = 0
 var fallen_rotation = 0;
 onready var Planet = get_parent().get_node("Planet")
 onready var sprite = $Sprite
@@ -15,18 +16,20 @@ func _ready():
 	contact_monitor = true
 	contacts_reported = 2
 
-func get_rotation_player(grav_vector):
-	var rotation = Vector2(0,1)
-	return rotation.angle_to(grav_vector)
+func get_rotation_player(vel_vector):
+	var rotation = Vector2(1,0)
+	return rotation.angle_to(vel_vector)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var gravity_vector = (grav_point - position).normalized()
-	var rotation = get_rotation_player(gravity_vector)
+	var linear_vel = get_linear_velocity()
+	if linear_vel.length() > 20:
+		b_rotation = get_rotation_player(linear_vel.normalized())
 	#if linear_velocity.y < 0:
 	#	fallen_rotation += 0.3
 	#	rotation += fallen_rotation
-	set_rotation(rotation)
+		set_rotation(b_rotation)
 
 	#if contact_monitor:
 	#	queue_free()
