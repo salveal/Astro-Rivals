16aec616df96930c56bdc0e9be1e545c53dfe891
letoro99 <atgmatias.12@gmail.com>
2022-05-17 23:12:57 -0400

    Implementacion de fin de turno de distintas formas y varios
    - Se implemento fin de turno por muerte del soldado activo
    - Se implemento fin de turno por misil fuera de campo visible
    - Se implemento fin de turno por colision del proyectil
    - Se implemento fin de turno por agotar tiempo del timer
    - Se implemento timer para turno de los equipos y un standby entre turnos
    - Se agrego sprites para indicar el equipo activo y standby

--- AstroRivals/scenes/soldiers/weapons/misil.gd
+++ AstroRivals/scenes/soldiers/weapons/misil.gd
@@ -8,6 +8,7 @@ onready var sprite = $Sprite
 onready var animTree = $AnimationTree
 onready var timer_damage = $Timer_damage
 onready var playback = animTree.get("parameters/playback")
+onready var soldierShot
 var EXPLOSION = preload("res://scenes/soldiers/weapons/explosion.tscn")
 var tick = 0
 var explosion
@@ -17,7 +18,13 @@ func _ready():
 	grav_point = Planet.global_position
 	playback.travel("init")
 	contact_monitor = true
-	
+
+# Funcion para asociar el proyectil con el soldado que ejecuto el disparo
+# Aqui se cambian las variables que no se pueden inicializar en _ready()
+func initMisil(soldier: Node):
+	soldierShot = soldier
+
+# Funcion para calcular la rotacion del misil con respecto al planeta asociado
 func get_rotation_player(vel_vector):
 	var rotation = Vector2(1,0)
 	return rotation.angle_to(vel_vector)
@@ -32,6 +39,7 @@ func _process(delta):
 		
 	check_colision()
 
+# Cheque si existe colision del misil para ejecutar su explosion/daño o no
 func check_colision():
 	var bodies = get_colliding_bodies()
 	if len(bodies) > 0 and tick == 0:
@@ -44,5 +52,13 @@ func check_colision():
 		tick+=1
 		
 	elif tick==1 and timer_damage.is_stopped():
-		queue_free()
+		deleteNode()
 		explosion.generate_damage()
+
+# Envia la señal para indicar que el misil choco y exploto al soldado
+func sendColisionToSoldier():
+	soldierShot.colisionShot()
+	
+func deleteNode():
+	sendColisionToSoldier()
+	queue_free()
